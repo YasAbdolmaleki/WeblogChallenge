7ab22bd2486d38a0a7ea05b9e2ced7679160abd0
Yasaman Abdolmaleki <Yasaman.Abdolmaleki@td.com>
2016-08-17 11:08:59 -0400

    aggregate all hits with fixed time window of 15mins

--- build.sbt
+++ build.sbt
@@ -3,5 +3,23 @@ name := "WeblogChallenge"
 version := "1.0"
 
 scalaVersion := "2.11.8"
-
+/*
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "2.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.0.0"
+libraryDependencies += "joda-time" % "joda-time" % "2.9.4"
+libraryDependencies += "log4j" % "log4j" % "1.2.17"
+libraryDependencies += "com.databricks" % "spark-csv_2.10" % "1.4.0"
+*/
+
+// additional libraries
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "1.3.1" % "provided",
+  "org.apache.spark" %% "spark-sql" % "1.3.1",
+  "org.apache.spark" %% "spark-hive" % "1.3.1",
+  "org.apache.spark" %% "spark-streaming" % "1.3.1",
+  "org.apache.spark" %% "spark-streaming-flume" % "1.3.1",
+  "org.apache.spark" %% "spark-mllib" % "1.3.1",
+  "org.scalatest" %% "scalatest" % "2.2.1" % "test"
+)
--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,9 +1,49 @@
 package main
 
-/**
-  * Created by sparkdev on 07/08/16.
-  */
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkContext, SparkConf}
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+
+case class Log(ip: String, timeMin: String, timeMax: String, backend:String )
+
 object Main extends App{
-  println("scala")
+
+  val conf = new SparkConf()
+    .setMaster("local[*]")  //take all available cores
+    .setAppName("WeblogChallenge")
+  val sc = new SparkContext(conf)
+  val logFile = sc.textFile("/home/sparkdev/challenge/WeblogChallenge/data/2015_07_22_mktplace_shop_web_log_sample.log") //FIXME: HARDCODED PATH  - RDD[ String ]
+
+  val sqlContext = new HiveContext(sc)
+
+  import sqlContext.implicits._
+
+  val rdd = logFile.map( line => line.split("\\s(?=([^\"]*\"[^\"]*\")*[^\"]*$)",-1))
+      .map(eventRecord => {
+      val datetime = DateTimeFormat.forPattern("yyyy-MM-dd\'T\'HH:mm:ss.SSSSSSZ").parseDateTime(eventRecord(0))
+      val time  = DateTime.parse(eventRecord(0), DateTimeFormat.forPattern("yyyy-MM-dd\'T\'HH:mm:ss.SSSSSSZ")).toString("hh:mm:ss")
+      val ipAddress = eventRecord(2)
+      val serverAddress = eventRecord(3)
+      Log(ipAddress, time, time, serverAddress )
+    })
+
+  var logTable = rdd.toDF()
+  logTable.registerTempTable("logTable")
+
+  val deltTimeTable = sqlContext.sql("""SELECT  T1.ip,
+                                          T1.timeMin,
+                                          T1.backend,
+                                          COALESCE((MIN(unix_timestamp(T2.timeMin, "hh:mm:ss")) -
+                                            unix_timestamp(T1.timeMin,"hh:mm:ss"))/60,0) AS TimeDiff
+                                 FROM    logTable T1
+                                         LEFT JOIN logTable T2
+                                             ON T1.ip = T2.ip AND (unix_timestamp(T2.timeMin, "hh:mm:ss") > unix_timestamp(T1.timeMin, "hh:mm:ss"))
+                                 GROUP BY T1.ip, T1.timeMin, T1.backend
+                                 ORDER BY T1.ip asc""")
+  deltTimeTable.registerTempTable("deltaTime")
+
+  val aggIP = sqlContext.sql("""SELECT ip,  backend from deltaTime where TimeDiff <= 15 Group by ip, backend""")
+  aggIP.show()
 
 }
